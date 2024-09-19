2e428ba0d658fcf2b8762ef730837a3cd89ea80c
tsarkoff <tsarkov.vladimir@gmail.com>
2024-09-19 23:14:07 +0300

    Unsuccessful attempt to use SAX callbacks to clean up an invalid XML.

--- src/main/java/ru/krymtech/Main.java
+++ src/main/java/ru/krymtech/Main.java
@@ -1,61 +1,83 @@
 package ru.krymtech;
 
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import java.io.IOException;
+import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-/**
- * Использованы материалы из RFC :
- * @rfc.org <a href="https://www.rfc-editor.org/rfc/rfc3076.txt">rfc3076</a>
- * 3.4 Character Modifications and Character References
- * @w3.org <a href="https://www.w3.org/TR/xml11/">xml11</a>
- * @Reserved Characters: These are characters that have special meanings within XML itself,
- * like angle brackets (<, >), the ampersand (&), and quotation marks (“, ‘).
- * If you try to use these characters directly in your data, they can confuse the XML parser and lead to errors.
- * @Valid Characters Allowed in XML
- * @Basic Latin Alphabet: All characters from the basic Latin alphabet (A-Z, a-z) are valid in XML.
- * @Digits: Numeric digits (0-9) are allowed.
- * @Special Characters: Certain special characters such as hyphen (-), underscore (_), period (.), colon (:), and comma (,), among others, are permitted in XML.
- * @Unicode Characters: XML supports a vast range of Unicode characters beyond the basic Latin alphabet, allowing representation of various languages, symbols, and emojis.
+/*
+ * Использованы материалы из RFC и w3.org:
+ * rfc.org https://www.rfc-editor.org/rfc/rfc3076.txt (3.4 Character Modifications and Character References)
+ * w3.org https://www.w3.org/TR/xml/ (2.2 Characters)
+ * Reserved Characters: These are characters that have special meanings within XML itself, like angle brackets (<, >), the ampersand (&), and quotation marks "",''.
+ * Basic Latin Alphabet: All characters from the basic Latin alphabet (A-Z, a-z) are valid in XML.
+ * Digits: Numeric digits (0-9) are allowed.
+ * Special Characters: Certain special characters such as hyphen (-), underscore (_), period (.), colon (:), and comma (,), among others, are permitted in XML.
+ * Unicode Characters: XML supports a vast range of Unicode characters beyond the basic Latin alphabet, allowing representation of various languages, symbols, and emojis.
  */
 
 public class Main {
-    private enum Method {
-        REPLACE_BY_CHAR,
-        REPLACE_BY_REGEX
-    }
-
     public static final String XML_FILENAME = "src/main/resources/forbidden_symbols.xml";
 
     public static void main(String[] args) throws IOException {
-        Method method = Method.REPLACE_BY_CHAR;
         byte[] bytes = Files.readAllBytes(Paths.get(XML_FILENAME));
         String fileString = new String(bytes, StandardCharsets.UTF_8);
-        String replacedXml = switch (method) {
-            case REPLACE_BY_CHAR -> replaceXmlSymbols(fileString);
-            case REPLACE_BY_REGEX -> replaceXmlString(fileString);
-        };
-        System.out.println(replacedXml);
+        fileString = replaceXmlSymbols(fileString);
+        System.out.println(fileString);
     }
 
     public static String replaceXmlSymbols(String input) {
-        StringBuilder sb = new StringBuilder(input.length());
-        for (char c : input.toCharArray())
-            if (c == 0x9            // == '\t' (9)
-                    || c == 0xA     // == '\n' (10)
-                    || c == 0xD     // == '\r' (13)
-                    || (c >= 0x20 && c <= 0xD7FF)
-                    || (c >= 0xE000 && c <= 0xFFFD)
-                    || (c >= 0x10000 && c <= 0x10FFFF)
-            ) sb.append(c);
-            else sb.append('_');
-
-        return sb.toString();
+        CorrectionHandler correctionHandler = new CorrectionHandler();
+        try {
+            SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
+            parser.parse(new InputSource(new StringReader(input)), correctionHandler);
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            System.out.println("Failed to parse XML: " + e.getMessage());
+        }
+        return correctionHandler.getCorrectedXml();
     }
 
-    public static String replaceXmlString(String input) {
-        String regex = "[^\\x09\\x0A\\x0D\\x20-\\uD7FF\\uE000-\\uFFFD\\u10000-\\u10FFFF]";
-        return input.replaceAll(regex, input);
+    private static class CorrectionHandler extends DefaultHandler {
+        private final StringBuilder sb = new StringBuilder();
+
+        @Override
+        // Doesn't work = it throws SAXException and stops parsing w/o possibility to continue XML assembling
+        public void characters(char[] ch, int start, int length) {
+            String content = new String(ch, start, length);
+            for (char c : content.toCharArray())
+                sb.append((c == '<' || c == '>' || c == '&' || c == '\"' || c == '\'' || c == ':') ? '_' : c);
+        }
+
+        @Override
+        public void ignorableWhitespace(char[] ch, int start, int length) {
+            sb.append(ch, start, length);
+        }
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes) {
+            sb.append("<").append(qName);
+            for (int i = 0; i < attributes.getLength(); i++) {
+                sb.append(" ").append(attributes.getQName(i)).append("=\"").append(attributes.getValue(i)).append("\"");
+            }
+            sb.append(">");
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String qName) {
+            sb.append("</").append(qName).append(">");
+        }
+
+        public String getCorrectedXml() {
+            return sb.toString();
+        }
     }
 }
